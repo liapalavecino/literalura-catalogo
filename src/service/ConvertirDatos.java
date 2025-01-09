646dc568b184f204d5e69aeeaccc1e046ebb70cf
Lia <soledadlia@gmail.com>
2025-01-09 03:55:57 -0300

    Se crea clase e interfaz ConvertirDatos para mapear datos de la API

---
+++ src/service/ConvertirDatos.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.DatosLibros;
+
+public class ConvertirDatos {
+    private ObjectMapper objectMapper = new ObjectMapper;
+
+    public DatosLibros obtenerDatos(String json)
+}
