8d0db251a9123bb86b22949219dd91d50c8e11a1
Lia <soledadlia@gmail.com>
2025-01-09 03:48:57 -0300

    Incluyendo la dependencia Jackson en pom.xml

--- src/main/java/com/challenge/literalura/LiteraluraApplication.java
+++ src/main/java/com/challenge/literalura/LiteraluraApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
